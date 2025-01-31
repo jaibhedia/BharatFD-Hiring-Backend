86f616139717f6de3592910896459d6516c12f8b
jaibhedia <jaibhedia@gmail.com>
2025-02-01 02:12:30 +0530

    refractor: app.js

--- multilang-faqs/src/app.js
+++ multilang-faqs/src/app.js
@@ -13,10 +13,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// API Routes
 app.use('/api/faqs', faqRoutes);
 
-// 404 Route Handling
+
 app.use((req, res) => res.status(404).json({ message: 'Route not found' }));
 
 module.exports = app;
